40c3606e714d38247f23c1c8523f2124b1932359
dchen <danielc204@deltalearns.ca>
2025-04-17 13:31:11 -0700

    New mob

--- Assets/Scripts/Combat/Melee/AttackScript.cs
+++ Assets/Scripts/Combat/Melee/AttackScript.cs
@@ -8,16 +8,17 @@ public class AttackScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        // Gets componenet from class "Locomotion" (script)
+        locomotion.GetComponent<Locomotion>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && locomotion.grounded == true)
+        if (Input.GetMouseButtonDown(0) && locomotion.grounded == true && locomotion.attacking == 0)
         {
             charAnimator.SetTrigger("attack");
-
+            locomotion.attacking += 0.1f;
         }
     }
 }
--- Assets/Scripts/Object properties/Destructables.cs
+++ Assets/Scripts/Object properties/Destructables.cs
@@ -4,17 +4,24 @@ public class Destructables : MonoBehaviour
 {
 
     [SerializeField] private LayerMask weapon;
+    [SerializeField] private Collider Collider;
     [SerializeField] private Rigidbody obj;
+    [SerializeField] private Locomotion locomotion;
     Vector3 movement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Gets componenet from class "Locomotion" (script)
+        locomotion.GetComponent<Locomotion>();
     }
 
     private void OnTriggerEnter(Collider weapon)
     {
-        movement = new Vector3(12,0,12);
-        obj.AddForce(movement);
+        if (locomotion.attacking >= 0.015f && locomotion.attacking <= 0.075f)
+        {
+            movement = new Vector3(222,0,222);
+            obj.AddForce(movement);
+        }
+
     }
 }
